ede6a4929225e649bc3822157a5b488c5bdb1062
ZahrahAhmed <zm99__@hotmail.com>
2017-11-06 19:26:14 +0300

    task2

---
+++ restaurants/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Restaurant
+
+def restaurant_list(request):
+	context = {
+	"objects": Restaurant.objects.all(),
+	}
+	return render(request, "restaurant_list.html", context)
+
+def restaurant_detail(request, restaurant_id):
+	context = {
+	"object": Restaurant.objects.get(id=restaurant_id),
+	}
+	return render(request, "restaurant_detail.html", context)
+
+
