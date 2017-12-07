522c82d60c6299b742c8b116ec38e36325633bbf
ZahrahAhmed <zm99__@hotmail.com>
2017-12-07 23:06:44 +0300

    fixed

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.RestaurantListView.as_view(), name="list"),
     url(r'^create/$', views.RestaurantCreateView.as_view(), name="create"),
-    url(r'^(?P<slug>[-\w]+)/$', views.RestaurantDetailView.as_view(), name="detail"),
+    url(r'^(?P<slug>[-\w]+)/detail/$', views.RestaurantDetailView.as_view(), name="detail"),
     url(r'^(?P<slug>[-\w]+)/delete/$', views.RestaurantDeleteView.as_view(), name="delete"),
     url(r'^(?P<slug>[-\w]+)/update/$', views.RestaurantUpdateView.as_view(), name="update"),
-    url(r'^(?P<slug>[-\w]+)/$', views.ItemDetailAPIView.as_view(), name="itemdetail"),
+    url(r'^(?P<slug>[-\w]+)/item/$', views.ItemDetailAPIView.as_view(), name="itemdetail"),
 ]
